23993f0ae4f45f22f43db6f47827e5f5e443b947
Kyle Chutskoff <kchutskoff@gmail.com>
2014-02-02 20:12:28 -0800

    Starting work on the flat file dbo connection. It will be replaced with a sql server-side DB later, but the functions and how they work will stay. Should make it async.

--- scripts/dbConnection.js
+++ scripts/dbConnection.js
@@ -1,24 +1,73 @@
 "use strict";
 
 function DBConnection(){
-	
-}
+	// for now, going to load everything synchronously.
+	var dbo = {};
 
-DBConnection.prototype.resolveTarget = function(target){
-	// with flat files, this requires the publicregistry and mapItem
-}
+	var loadFlatFile = function(fileName){
+		$.ajax({
+			url: "data/" + fileName + ".json",
+			async: false,
+			dataType: "json",
+			success: function(data){
+				dbo[fileName] = data;
+			}
+			error: function(jqXHR, textStatus, errorThrown){
+				console.warn("Failed to get " + fileName + ".json - " + textStatus + " " + errorThrown);
+			}
+		});
+	}
 
-// items is optional, including an array of strings (or one string) will only load those types
-// not specifying items will load all items
-DBConnection.prototype.getZoneItems = function(zone, items){
-	// with flat files, this requires the map
-}
+	loadFlatFile("dboMapInfo");
+	loadFlatFile("dboMapItem");
+	loadFlatFile("dboMapRegion");
+	loadFlatFile("dboMapZone");
+	loadFlatFile("dboPublicRegistry");
 
-// gets everything required for the map to load
-DBConnection.prototype.getMapInfo = function(){
-	// returns map size and regions
-}
+	// returns an object name and position used to initialize the map (or null if doesn't exist)
+	this.resolveTarget = function(target){
+		// with flat files, this requires the publicregistry and mapItem
+		var dboMapItem  = dbo["dboMapItem"];
+		var dboMapZone  = dbo["dboMapZone"];
+		var dboMapRegion  = dbo["dboMapRegion"];
+		var dboPublicRegistry = dbo["dboPublicRegistry"];
 
-DBConnection.prototype.getZoneRoutes = function(zone){
-	
+		if(target in dboPublicRegistry){
+			entry = dboPublicRegistry[target];
+			switch(entry){
+				case "item":
+				var item = dboMapItem[entry.localid];
+				return {name: item.name, pos: item.pos};
+				break;
+
+				case "zone":
+				var zone = dboMapZone[entry.localid];
+				return {name: zone.name, pos: {x: (zone.area.right + zone.area.left) / 2, y: (zone.area.bottom + zone.area.top) / 2}};
+				break;
+
+				case "region":
+				var region = dboMapRegion[entry.localid];
+				return {name: region.name, pos: region.label};
+				break;
+
+				default:
+				return null;
+			}
+		}
+	}
+
+	// items is optional, including an array of strings (or one string) will only load those types
+	// not specifying items will load all items
+	this.getZoneItems = function(zone, items){
+		// with flat files, this requires the map
+	}
+
+	// gets everything required for the map to load
+	this.getMapInfo = function(){
+		// returns map size and regions
+	}
+
+	this.getZoneRoutes = function(zone){
+		
+	}
 }
